2f25ed2b5b2cb99fb0aa35ac7264b43a1190e145
Antonio Melendez <mel23001@byui.edu>
2025-01-23 22:36:46 -0600

    get all and get single ready

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,7 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {res.send('Hello World')});
 
+router.use('/users', require('./users'));
+router.use('/comments', require('./comments'));
+
 module.exports = router;
