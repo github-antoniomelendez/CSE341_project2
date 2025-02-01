b7736bf5011906be7a7a3a115abe013470c1daf9
Antonio Melendez <mel23001@byui.edu>
2025-01-31 19:25:37 -0600

    logout

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.use('/comments', require('./comments'));
 
 router.get('/login', passport.authenticate('github'), (req, res) => {});
 
-router.get('logout', function(req, res, next) {
+router.get('/logout', function(req, res, next) {
     req.logout(function(err) {
         if (err) { return next(err); }
         res.redirect('/');
